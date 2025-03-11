c081b3fae0f5316fcad3aecf035ce58e39e43fa5
HPDQ\nguyenvotuannhan <nguyenvotuannhan@hoaphat.com.vn>
2025-03-11 15:42:32 +0700

    change controller name

---
+++ Data_Product/Controllers/BM16_GangThoiController.cs
@@ -0,0 +1,171 @@
+﻿using Data_Product.Models;
+using Data_Product.Repositorys;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using MySql.Data.MySqlClient;
+using System.Security.Claims;
+
+namespace Data_Product.Controllers
+{
+    public class BieuMau16Controller : Controller
+    {
+        private readonly DataContext _context;
+        private readonly ICompositeViewEngine _viewEngine;
+
+        public BieuMau16Controller(DataContext _context, ICompositeViewEngine viewEngine)
+        {
+            this._context = _context;
+            _viewEngine = viewEngine;
+        }
+        public ActionResult TaoPhieu()
+        {
+            DateTime DayNow = DateTime.Now;
+            String Day = DayNow.ToString("dd/MM/yyyy");
+            DateTime NgayLamViec = DateTime.ParseExact(Day, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None);
+
+            var TenTaiKhoan = User.FindFirstValue(ClaimTypes.Name);
+            var TaiKhoan = _context.Tbl_TaiKhoan.Where(x => x.TenTaiKhoan == TenTaiKhoan).FirstOrDefault();
+            var PhongBan = _context.Tbl_PhongBan.Where(x => x.ID_PhongBan == TaiKhoan.ID_PhongBan).FirstOrDefault();
+            string TenBP = PhongBan.TenNgan.ToString();
+
+            List<Tbl_NhomVatTu> vt = _context.Tbl_NhomVatTu.ToList();
+            ViewBag.VTList = new SelectList(vt, "ID_NhomVatTu", "TenNhomVatTu");
+
+
+            List<Tbl_PhongBan> pb = _context.Tbl_PhongBan.ToList();
+            ViewBag.ID_PhongBan = new SelectList(pb, "ID_PhongBan", "TenPhongBan");
+
+            var NhanVien = (from a in _context.Tbl_TaiKhoan
+                                  select new Tbl_TaiKhoan
+                                  {
+                                      ID_TaiKhoan = a.ID_TaiKhoan,
+                                      HoVaTen = a.TenTaiKhoan + " - " + a.HoVaTen
+                                  }).ToList();
+
+            ViewBag.IDTaiKhoan = new SelectList(NhanVien, "ID_TaiKhoan", "HoVaTen");
+
+            var VatTu = (from a in _context.Tbl_VatTu.Where(x => x.PhongBan.Contains(TenBP) && x.ID_TrangThai == 1)
+                               select new Tbl_VatTu
+                               {
+                                   ID_VatTu = a.ID_VatTu,
+                                   TenVatTu = a.TenVatTu
+                               }).ToList();
+
+            ViewBag.VTList = new SelectList(VatTu, "ID_VatTu", "TenVatTu");
+
+            var MaLo = (from a in _context.Tbl_MaLo
+                              select new Tbl_MaLo
+                              {
+                                  ID_MaLo = a.ID_MaLo,
+                                  TenMaLo = a.TenMaLo
+                              }).ToList();
+
+            ViewBag.MLList = new SelectList(MaLo, "ID_MaLo", "TenMaLo");
+
+            var CaKip = (from a in _context.Tbl_Kip.Where(x => x.NgayLamViec == NgayLamViec)
+                               select new Tbl_Kip
+                               {
+                                   ID_Kip = a.ID_Kip,
+                                   TenCa = a.TenCa
+                               }).ToList();
+            ViewBag.IDKip = new SelectList(CaKip, "ID_Kip", "TenCa");
+
+
+
+            var NV = (from a in _context.Tbl_TaiKhoan
+                            select new Tbl_TaiKhoan
+                            {
+                                ID_TaiKhoan = a.ID_TaiKhoan,
+                                HoVaTen = a.TenTaiKhoan + " - " + a.HoVaTen
+                            }).ToList();
+
+            ViewBag.IDNhanVien = new SelectList(NV, "ID_TaiKhoan", "HoVaTen");
+
+            //DateTime date = new DateTime(2025, 3, 10);
+            //var json = GetSoMeGangBKMis(date.ToString("yyyy-MM-dd"), 1, "2");
+
+
+            return PartialView();
+        }
+
+        public async Task<IActionResult> GetSoMeGangBKMis(string ngay, int? ID_LoCao, string IDKip)
+        {
+            string cakip = "";
+            if (IDKip != null)
+            {
+                var dt = DateTime.Parse(ngay);
+                var ca = _context.Tbl_Kip.FirstOrDefault(x => x.TenCa == IDKip && x.NgayLamViec == dt);
+                cakip = ca?.TenCa + ca?.TenKip;
+            }
+            var dvt = new List<Bkmis_view>();
+            // Chuỗi kết nối MySQL
+            // string connectionString = "Server=10.192.215.11,3307;Database=bkmis_kcshpsdq;User Id=viewkcs;Password=viewkcs@2024;";
+            string connectionString = "Server=10.192.215.11;Database=bkmis_kcshpsdq;User Id=viewkcs;Password=viewkcs@2024;Port=3307;";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("Kết nối thành công!");
+
+                    // Câu lệnh SQL cần thực thi
+                    string query = "SELECT TestPatternCode,ClassifyName,ProductionDate,ShiftName,InputTime,Patterntime,TestPatternName " +
+                        "FROM bkmis_kcshpsdq.view_dq1_lg_daura_lc1 " +
+                        "where bkmis_kcshpsdq.view_dq1_lg_daura_lc1.ProductionDate = '" +
+                         ngay + "'" + " and bkmis_kcshpsdq.view_dq1_lg_daura_lc1.ShiftName ='" + cakip + "'";
+
+                    if (ID_LoCao == 2)
+                    {
+                        query = "SELECT TestPatternCode,ClassifyName,ProductionDate,ShiftName,InputTime,Patterntime,TestPatternName " +
+                        "FROM bkmis_kcshpsdq.view_dq1_lg_daura_lc2 " +
+                        "where bkmis_kcshpsdq.view_dq1_lg_daura_lc2.ProductionDate = '" +
+                         ngay + "'" + " and bkmis_kcshpsdq.view_dq1_lg_daura_lc1.ShiftName ='" + cakip + "'";
+                    }
+                    else if (ID_LoCao == 3)
+                    {
+                        query = "SELECT TestPatternCode,ClassifyName,ProductionDate,ShiftName,InputTime,Patterntime,TestPatternName " +
+                        "FROM bkmis_kcshpsdq.view_dq1_lg_daura_lc3 " +
+                        "where bkmis_kcshpsdq.view_dq1_lg_daura_lc3.ProductionDate = '" +
+                         ngay + "'" + " and bkmis_kcshpsdq.view_dq1_lg_daura_lc1.ShiftName ='" + cakip + "'";
+                    }
+                    else if (ID_LoCao == 4)
+                    {
+                        query = "SELECT TestPatternCode,ClassifyName,ProductionDate,ShiftName,InputTime,Patterntime,TestPatternName " +
+                       "FROM bkmis_kcshpsdq.view_dq1_lg_daura_lc4 " +
+                       "where bkmis_kcshpsdq.view_dq1_lg_daura_lc4.ProductionDate = '" +
+                        ngay + "'" + " and bkmis_kcshpsdq.view_dq1_lg_daura_lc1.ShiftName ='" + cakip + "'";
+                    }
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read()) // Đọc từng dòng dữ liệu
+                            {
+                                //Console.WriteLine($"ID: {reader["Id"]}, Name: {reader["Name"]}, Age: {reader["Age"]}");
+                                dvt.Add(new Bkmis_view()
+                                {
+                                    ClassifyName = reader["ClassifyName"].ToString(),
+                                    InputTime = reader["InputTime"].ToString(),
+                                    Patterntime = reader["Patterntime"].ToString(),
+                                    ShiftName = reader["ShiftName"].ToString(),
+                                    ProductionDate = reader["ProductionDate"].ToString(),
+                                    TestPatternCode = reader["TestPatternCode"].ToString(),
+                                    TestPatternName = reader["TestPatternName"].ToString(),
+                                });
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Lỗi: " + ex.Message);
+                }
+            }
+
+            return Json(dvt);
+        }
+    }
+}
