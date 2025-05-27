19035e1e65a70e7b1de4129447b190e326673ef7
nvp98 <vinhphuoc1892@gmail.com>
2025-05-27 15:33:49 +0700

    create GangThoi_Thep

---
+++ Data_Product/Controllers/BM_GangThoi_ThepController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Data_Product.Controllers
+{
+    public class BM_GangThoi_ThepController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
