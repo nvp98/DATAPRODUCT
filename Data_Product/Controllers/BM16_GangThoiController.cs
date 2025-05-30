7e780773709c3aef493aa239a94c904ef30060ef
ThanhSangLe <lethanhsang15052025@gmail.com>
2025-05-30 07:54:58 +0700

    Task_BM16_GangThoi |  Tạo controller và view danh sach phieu

--- Data_Product/Controllers/BM16_GangThoiController.cs
+++ Data_Product/Controllers/BM16_GangThoiController.cs
@@ -7,10 +7,133 @@ using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.EntityFrameworkCore;
 using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 
+public class PhieuGiaoNhanViewModel
+{
+    public string MaPhieu { get; set; }
+    public string Ngay { get; set; }
+    public string Ca { get; set; }
+    public string LoCao { get; set; }
+    public string BoPhanTao { get; set; }
+    public string NguoiTao { get; set; }
+    public string ThoiGianTao { get; set; }
+}
+public class ThungGang
+{
+    [Key]
+    [StringLength(20)]
+    public string MaThung { get; set; }
+
+    [StringLength(20)]
+    public string MaPhieu { get; set; }
+
+    [StringLength(20)]
+    public string SoMe { get; set; }
+
+    public int ThungSo { get; set; }
+
+    public DateTime Gio { get; set; }
+
+    [StringLength(20)]
+    public string PhanLoai { get; set; }
+
+    public int MaLoCao { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime Ngay { get; set; }
+
+    [StringLength(10)]
+    public string MaCaKip { get; set; }
+
+    [StringLength(10)]
+    public string MaXeGoong { get; set; }
+
+    public float KLThungVaGangLong { get; set; }
+
+    public float KLGangLong { get; set; }
+
+    [NotMapped]
+    public float KLGang => KLGangLong - KLThungVaGangLong;
+
+    [StringLength(50)]
+    public string NoiDen { get; set; }
+
+    public DateTime GioTaoPhieu { get; set; }
+
+    [StringLength(20)]
+    public string TrangThai { get; set; } = "ChuaChuyen";
+
+    [StringLength(10)]
+    public string MaNhanVienTao { get; set; }
+
+    [StringLength(10)]
+    public string BoPhan { get; set; }
+
+    [StringLength(10)]
+    public string MaNhanVienNhan { get; set; }
+
+    public DateTime? ThoiGianNhan { get; set; }
+
+    [StringLength(10)]
+    public string MaLoThoi { get; set; }
+
+    [StringLength(20)]
+    public string MaMeThoi { get; set; }
+
+    [StringLength(10)]
+    public string NguoiChot { get; set; }
+
+    [StringLength(10)]
+    public string BoPhanChot { get; set; }
+
+    public DateTime? ThoiGianChot { get; set; }
+}
+public class Phieu
+{
+    [Key]
+    [StringLength(20)]
+    public string MaPhieu { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime Ngay { get; set; }
+
+    [StringLength(10)]
+    public string MaCaKip { get; set; }
+
+    [StringLength(10)]
+    public string MaTo { get; set; }
+
+    [StringLength(10)]
+    public string MaLoCao { get; set; }
+
+    [StringLength(10)]
+    public string NguoiLap { get; set; }
+
+    public DateTime GioTao { get; set; }
+
+    [StringLength(20)]
+    public string TrangThai { get; set; } = "ChuaHoanTat";
+
+    [StringLength(10)]
+    public string NguoiChot { get; set; }
+
+    public DateTime? ThoiGianChot { get; set; }
+
+    [StringLength(10)]
+    public string BoPhanTao { get; set; }
+
+    [StringLength(10)]
+    public string BoPhanChot { get; set; }
+
+    // Navigation property - Danh sách các thùng gang thuộc phiếu này
+    public virtual ICollection<ThungGang> DanhSachThungGang { get; set; }
+}
 namespace Data_Product.Controllers
 {
+    
     public class BM16_GangThoiController : Controller
     {
         private readonly DataContext _context;
@@ -120,6 +243,24 @@ namespace Data_Product.Controllers
         public IActionResult Edit_Demo()
         {
             return View(); // Sẽ tìm view: Views/BM16_GangThoi/Edit_Demo.cshtml
+        }
+
+        public async Task<IActionResult>Danhsachphieu()
+        {
+            var danhSachPhieu = new List<PhieuGiaoNhanViewModel>
+            {
+                new PhieuGiaoNhanViewModel { MaPhieu = "GN001", Ngay = "25/05/2025", Ca = "Ca ngày", LoCao = "Lò 3", BoPhanTao = "Phân xưởng Gang", NguoiTao = "Nguyễn Văn A", ThoiGianTao = "08:30:15" },
+                new PhieuGiaoNhanViewModel { MaPhieu = "GN002", Ngay = "25/05/2025", Ca = "Ca ngày", LoCao = "Lò 2", BoPhanTao = "Phân xưởng Thép", NguoiTao = "Trần Thị B", ThoiGianTao = "08:45:20" },
+                new PhieuGiaoNhanViewModel { MaPhieu = "GN003", Ngay = "24/05/2025", Ca = "Ca ngày", LoCao = "Lò 1", BoPhanTao = "Phân xưởng Gang", NguoiTao = "Phạm Văn C", ThoiGianTao = "21:10:50" },
+                new PhieuGiaoNhanViewModel { MaPhieu = "GN004", Ngay = "23/05/2025", Ca = "Ca đêm", LoCao = "Lò 3", BoPhanTao = "Phân xưởng Thép", NguoiTao = "Đặng Thị D", ThoiGianTao = "14:05:12" },
+                new PhieuGiaoNhanViewModel { MaPhieu = "GN005", Ngay = "22/05/2025", Ca = "Ca ngày", LoCao = "Lò 2", BoPhanTao = "Phân xưởng Gang", NguoiTao = "Hoàng Văn E", ThoiGianTao = "07:55:33" },
+            };
+            return View(danhSachPhieu);
+        }
+
+        public async Task<IActionResult> TaoPhieu()
+        {
+            return View();
         }
     }
 }
