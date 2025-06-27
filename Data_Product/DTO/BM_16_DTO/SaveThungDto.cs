38f3d4153eab99de4ea51a2f4cd96c1b7d499104
ThanhSangLe <lethanhsang15052025@gmail.com>
2025-06-27 15:46:45 +0700

    Task_BM16 | Fix ngaytao

--- Data_Product/DTO/BM_16_DTO/SaveThungDto.cs
+++ Data_Product/DTO/BM_16_DTO/SaveThungDto.cs
@@ -7,6 +7,7 @@ namespace Data_Product.DTO.BM_16_DTO
         public string MaPhieu { get; set; }
         public int ID_Locao { get; set; }
         public int ID_Kip { get; set; }
+        public DateTime NgayPhieuGang { get; set; }
         [JsonPropertyName("DanhSachThung")]
         public List<ThungGangDto> DanhSachThung { get; set; }
     }
