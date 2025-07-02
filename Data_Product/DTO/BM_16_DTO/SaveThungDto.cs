f7ae9bfba994c241cf06dba6dd48fad4ec3dff54
ThanhSangLe <lethanhsang15052025@gmail.com>
2025-07-02 16:58:46 +0700

    Task_BM16 | Fixbug PKH

--- Data_Product/DTO/BM_16_DTO/SaveThungDto.cs
+++ Data_Product/DTO/BM_16_DTO/SaveThungDto.cs
@@ -5,10 +5,18 @@ namespace Data_Product.DTO.BM_16_DTO
     public class SaveThungDto
     {
         public string MaPhieu { get; set; }
+        [JsonPropertyName("ID_LoCao")]
         public int ID_Locao { get; set; }
         public int ID_Kip { get; set; }
         public DateTime NgayPhieuGang { get; set; }
         [JsonPropertyName("DanhSachThung")]
         public List<ThungGangDto> DanhSachThung { get; set; }
+    }
+
+    public class UpdateThungRequest
+    {
+        public string MaPhieu { get; set; }
+        public List<ThungGangDto> DanhSachThung { get; set; }
+
     }
 }
--- Data_Product/DTO/BM_16_DTO/ThungGangCreateDto.cs
+++ Data_Product/DTO/BM_16_DTO/ThungGangCreateDto.cs
@@ -12,21 +12,21 @@ namespace Data_Product.DTO.BM_16_DTO
         public string? BKMIS_SoMe { get; set; }
       
         public string? BKMIS_ThungSo { get; set; }
-        [Required]
+     
         public string? BKMIS_Gio { get; set; }
-        [Required]
+      
         public string? BKMIS_PhanLoai { get; set; }
-        [Required]
+       
         public decimal? KL_XeGoong { get; set; }
-        [Required]
+        
         public decimal? G_KLThungChua { get; set; }
-        [Required]
+       
         public decimal? G_KLThungVaGang { get; set; }
-        [Required]
+      
         public decimal? G_KLGangLong { get; set; }
-        [Required]
+       
         public string? ChuyenDen { get; set; }
-        [Required]
+       
         public string? Gio_NM { get; set; }
         public string G_GhiChu { get; set; }
 
