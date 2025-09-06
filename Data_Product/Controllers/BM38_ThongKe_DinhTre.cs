c3e23223dad64941876c9bcadb3f4744ca823eae
nvp98 <vinhphuoc1892@gmail.com>
2025-09-06 08:54:18 +0700

    add UI BM 38

---
+++ Data_Product/Controllers/BM38_ThongKe_DinhTre.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Data_Product.Controllers
+{
+    public class BM38_ThongKe_DinhTreController : Controller
+    {
+        public async  Task<IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> View_Details()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Edit()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ExportPdfView(int id)
+        {
+            return View("ExportPdfView", new { id = id });
+        }
+
+
+    }
+}
