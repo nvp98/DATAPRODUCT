507c8c8b39a7d49e168b077b196b10394a7f888b
nvp98 <vinhphuoc1892@gmail.com>
2025-09-16 08:59:57 +0700

    Fix check xuong, fillter xuong

--- Data_Product/Controllers/BM_NhatKy_SanXuatController.cs
+++ Data_Product/Controllers/BM_NhatKy_SanXuatController.cs
@@ -288,10 +288,10 @@ namespace Data_Product.Controllers
             if (ID_TrangThai != null) query = query.Where(x => x.TinhTrang == ID_TrangThai);
             if (IDCa != null) query = query.Where(x => x.Ca == IDCa);
             if (IDPhongBan != null) query = query.Where(x => x.ID_PhongBan_SX == IDPhongBan);
-            if (IDXuong != null) query = query.Where(x => _context.Tbl_NhatKy_SanXuat_ChiTiet.Any(a => a.ID_Xuong == IDXuong));
-            if (LyDoDung != null) query = query.Where(x => _context.Tbl_NhatKy_SanXuat_ChiTiet.Any(a => a.LyDo_DungThietBi == LyDoDung));
+            if (IDXuong != null) query = query.Where(x => _context.Tbl_NhatKy_SanXuat_ChiTiet.Any(a => a.ID_NhatKy == x.ID && a.ID_Xuong == IDXuong));
+            if (LyDoDung != null) query = query.Where(x => _context.Tbl_NhatKy_SanXuat_ChiTiet.Any(a => a.ID_NhatKy == x.ID && a.LyDo_DungThietBi == LyDoDung));
             if (startDay != default && endDay != default) query = query.Where(x => x.NgayDungSX >= startDay && x.NgayDungSX <= endDay);
-            if (noidungDung != null) query = query.Where(x => _context.Tbl_NhatKy_SanXuat_ChiTiet.Any(a => (!string.IsNullOrEmpty(a.NoiDungDung) && a.NoiDungDung.ToLower().Contains(noidungDung.ToLower()))));
+            if (noidungDung != null) query = query.Where(x => _context.Tbl_NhatKy_SanXuat_ChiTiet.Any(a => a.ID_NhatKy == x.ID && (!string.IsNullOrEmpty(a.NoiDungDung) && a.NoiDungDung.ToLower().Contains(noidungDung.ToLower()))));
 
             // Thực thi truy vấn tại đây
             var res = await query.OrderByDescending(x => x.NgayDungSX).ToListAsync();
